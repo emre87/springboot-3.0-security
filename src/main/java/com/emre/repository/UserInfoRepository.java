a9bc57841999a5ac392418e46a253c9f7ef3379e
emre demiralay <emre@mail>
2023-07-17 22:29:09 +0300

    first commit

---
+++ src/main/java/com/emre/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.emre.repository;
+
+import com.emre.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
